a7f055cc651fcb6ed85a9bf4a18ede287efd1838
KKG <kkg05102@gmail.com>
2022-03-16 21:34:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import re
+import requests
 from PySide6.QtWidgets import QApplication, QMainWindow
 from bs4 import BeautifulSoup
-
-from mainwindow import Ui_MainWindow
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium import webdriver
+
+from mainwindow import Ui_MainWindow
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -27,9 +29,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         for v in el:
             self.comboBox.addItem(v)
 
-        self.pushButton.clicked.connect(self.button_clicked)
+        self.pushButton.clicked.connect(self.edit)
+        self.pushButton_2.clicked.connect(self.examine)
 
-    def button_clicked(self):
+    def edit(self):
 
         self.label_2.setText('editing..')
         self.progressBar.setVisible(True)
@@ -37,8 +40,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         index = self.comboBox.currentIndex() + 1
         driver.get('https://www.acmicpc.net/step')
-        driver.find_element(By.CSS_SELECTOR, 'body > div.wrapper > div.container.content > div:nth-child(5) > div > '
-                                             f'div > table > tbody > tr:nth-child({index}) > td:nth-child(2) > a').click()
+        driver.find_element(By.CSS_SELECTOR, 'body > div.wrapper > div.container.content > div:nth-child(5) '
+                                             '> div > div > table > tbody > '
+                                             f'tr:nth-child({index}) > td:nth-child(2) > a').click()
 
         req = driver.page_source
 
@@ -66,16 +70,19 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.progressBar.setValue(cur_value)
 
         self.text += '\n---\n\n### ' + self.comboBox.currentText() + '\n\n| 난이도 | 번호 | 이름 | 날짜 | 체크 ' \
-                                                                  '|\n|:---:|:---:|:---:|:---:| :---: |\n '
+                                                                     '|\n|:---:|:---:|:---:|:---:| :---: |\n '
         self.link += '\n'
 
         for a in range(len(self.num)):
-            self.text += f'| <img src="{self.difficulty[a]}" width="20px" height="25px"></img> | [{self.num[a]}][{self.num[a]}] | {self.name[a]} |  |  |\n'
+            self.text += f'| <img src="{self.difficulty[a]}" width="20px" height="25px"></img> ' \
+                         f'| [{self.num[a]}][{self.num[a]}] | {self.name[a]} |  |  |\n'
             self.link += f'[{self.num[a]}]: https://www.acmicpc.net/problem/{self.num[a]}\n'
-        self.content += f'| [{self.comboBox.currentText()}](#{self.comboBox.currentText().replace(", ", "-").replace(" ", "-")}) |\n'
-        self.edit()
+        self.content += f'| [{self.comboBox.currentText()}]' \
+                        f'(#{self.comboBox.currentText().replace(", ", "-").replace(" ", "-")}) |\n'
 
-    def edit(self):
+        self.write()
+
+    def write(self):
         with open('README1.md', 'r') as f:
             lines = f.readlines()
         with open('README2.md', 'w') as f:
@@ -90,7 +97,46 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                     f.write(line)
             f.write(self.link)
 
-        self.label_2.setText('success!')
+        self.label_2.setText('Success!')
+        self.progressBar.setValue(100)
+
+    def examine(self):
+        with open('README1.md', 'r') as f:
+            lines = f.readlines()
+
+        li = []
+
+        self.label_2.setText('examining..')
+        self.progressBar.setVisible(True)
+        self.progressBar.setValue(0)
+
+        progress_value = 100 / len(lines)
+        cur_value = 0
+
+        for line in lines:
+            if line.rstrip()[:48] == '| <img src="https://static.solved.ac/tier_small/':
+
+                num = re.findall('[0-9]+', line)[3]
+                dif = re.findall('[0-9]+', line)[0]
+
+                webpage = requests.get(f'https://solved.ac/search?query={num}')
+                soup = BeautifulSoup(webpage.content, 'html.parser')
+
+                difficulty = re.search('[0-9]+', soup.img.__getitem__(key='src')).group()
+
+                if dif != difficulty:
+                    li.append('(' + num + ') ' + line.split(' | ')[-3])
+
+            cur_value += progress_value
+            self.progressBar.setValue(int(cur_value))
+
+        if not li:
+            self.label_2.setText('Success! No wrong difficulty.')
+        else:
+            self.label_2.setText('Success!\n'
+                                 'Problem:\n\t'
+                                 ',\n\t'.join(li))
+
         self.progressBar.setValue(100)
 
 
@@ -102,7 +148,8 @@ el = []
 
 for i in range(1, 51):
     el.append(driver.find_element(By.CSS_SELECTOR,
-                                  f'body > div.wrapper > div.container.content > div:nth-child(5) > div > div > table > tbody > tr:nth-child({i}) > td:nth-child(2) > a').text)
+                                  f'body > div.wrapper > div.container.content > div:nth-child(5) > '
+                                  f'div > div > table > tbody > tr:nth-child({i}) > td:nth-child(2) > a').text)
 
 app = QApplication()
 window = MainWindow()
