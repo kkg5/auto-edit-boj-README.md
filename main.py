6b25c30dc6b9ce56fdf354d62a4433c5013ac8da
KKG <kkg05102@gmail.com>
2022-03-17 11:59:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -38,13 +38,18 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.progressBar.setVisible(True)
         self.progressBar.setValue(0)
 
-        index = self.comboBox.currentIndex() + 1
+        index = self.comboBox.currentIndex() + START_INDEX
+
+        driver = webdriver.Chrome(service=Service('./chromedriver'))
+        driver.set_window_rect(0, 0, 0, 0)
         driver.get('https://www.acmicpc.net/step')
         driver.find_element(By.CSS_SELECTOR, 'body > div.wrapper > div.container.content > div:nth-child(5) '
                                              '> div > div > table > tbody > '
                                              f'tr:nth-child({index}) > td:nth-child(2) > a').click()
 
         req = driver.page_source
+
+        driver.close()
 
         soup = BeautifulSoup(req, 'html.parser')
         self.num = []
@@ -55,32 +60,35 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         cur_value = 0
 
         for n in self.num:
-            driver.get(f'https://solved.ac/search?query={n}')
-            req = driver.page_source
-            soup = BeautifulSoup(req, 'html.parser')
-            v = soup.select('#__next > div.contents > div:nth-child(4) > div:nth-child(2) > div > '
-                            'div.StickyTable__Wrapper-sc-45ty5n-3.cerLvn.sticky-table > div > div:nth-child(2) > '
-                            'div:nth-child(1) > span > a > img')
-            self.difficulty.append(v[0].__getitem__(key='src'))
-            v = soup.select('#__next > div.contents > div:nth-child(4) > div:nth-child(2) > div > '
-                            'div.StickyTable__Wrapper-sc-45ty5n-3.cerLvn.sticky-table > div > div:nth-child(2) > '
-                            'div:nth-child(2) > span > a > span')
+            webpage = requests.get(f'https://solved.ac/search?query={n}')
+            soup = BeautifulSoup(webpage.content, 'html.parser')
+
+            self.difficulty.append(re.search('[0-9]+', soup.img.__getitem__(key='src')).group())
+
+            v = soup.select('div:nth-child(2) > div:nth-child(2) > span > a > span')
+
             self.name.append(v[0].getText())
+
             cur_value += progress_value
             self.progressBar.setValue(cur_value)
+
+        self.build_str()
+        self.write()
+
+    def build_str(self):
 
         self.text += '\n---\n\n### ' + self.comboBox.currentText() + '\n\n| 난이도 | 번호 | 이름 | 날짜 | 체크 ' \
                                                                      '|\n|:---:|:---:|:---:|:---:| :---: |\n '
         self.link += '\n'
 
+        print(self.difficulty)
         for a in range(len(self.num)):
             self.text += f'| <img src="{self.difficulty[a]}" width="20px" height="25px"></img> ' \
                          f'| [{self.num[a]}][{self.num[a]}] | {self.name[a]} |  |  |\n'
             self.link += f'[{self.num[a]}]: https://www.acmicpc.net/problem/{self.num[a]}\n'
+
         self.content += f'| [{self.comboBox.currentText()}]' \
                         f'(#{self.comboBox.currentText().replace(", ", "-").replace(" ", "-")}) |\n'
-
-        self.write()
 
     def write(self):
         with open('README1.md', 'r') as f:
@@ -115,9 +123,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         for line in lines:
             if line.rstrip()[:48] == '| <img src="https://static.solved.ac/tier_small/':
-
-                num = re.findall('[0-9]+', line)[3]
-                dif = re.findall('[0-9]+', line)[0]
+                nums = re.findall('[0-9]+', line)
+                num = nums[3]
+                dif = nums[0]
 
                 webpage = requests.get(f'https://solved.ac/search?query={num}')
                 soup = BeautifulSoup(webpage.content, 'html.parser')
@@ -146,10 +154,14 @@ driver.get('https://www.acmicpc.net/step')
 
 el = []
 
-for i in range(1, 51):
+START_INDEX = 1
+
+for i in range(START_INDEX, 51):
     el.append(driver.find_element(By.CSS_SELECTOR,
                                   f'body > div.wrapper > div.container.content > div:nth-child(5) > '
                                   f'div > div > table > tbody > tr:nth-child({i}) > td:nth-child(2) > a').text)
+
+driver.close()
 
 app = QApplication()
 window = MainWindow()
